8f197fa38df0b735320d9d7f32d399eb764de656
Víctor M. Reyes <vm.reyesal@gmail.com>
2020-04-16 17:17:53 -0600

    Añadiendo carpeta build

---
+++ build/precache-manifest.763b0cbd91a0328395fe9b525f15a7e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6770c5a5f6b842fba96f7f19f7e9e43b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87735e410a10c8983e0e",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "fa18a4a35696a6f3cf65",
+    "url": "/static/css/main.789095a8.chunk.css"
+  },
+  {
+    "revision": "87735e410a10c8983e0e",
+    "url": "/static/js/2.f38184dc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.f38184dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa18a4a35696a6f3cf65",
+    "url": "/static/js/main.3296a570.chunk.js"
+  },
+  {
+    "revision": "9b3c835a8cf5e5832c82",
+    "url": "/static/js/runtime-main.677cf86f.js"
+  }
+]);
